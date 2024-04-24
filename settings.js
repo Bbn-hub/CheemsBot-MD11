9d504c3a979af1abf5a79dea65867f8310b1fd2a
Demzzy <155068004+Bbn-hub@users.noreply.github.com>
2024-04-24 09:34:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
-//base by DGXeon
+//base by Demzzy
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
+//YouTube: @Demzzy
+//Instagram: Demzzy
+//Telegram: t.me/Demzzy
+//GitHub: @Demzzy
+//WhatsApp: +2349091878122 
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
 const fs = require('fs')
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: Demzzy" //ur yt chanel name
+global.socialm = "GitHub: Demzzy" //ur github or insta name
+global.location = "Africa, Nigeria, Lagos" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.botname = 'Demzzybugbot_md' //ur bot name
+global.ownernumber = '2349091878122' //ur owner number
+global.ownername = '🗿Demzzy' //ur owner name
+global.websitex = "null"
+global.wagc = "https://wa.me/2349091878122"
+global.themeemoji = '🥇❤️‍🩹'
+global.wm = "Demzzy bot incorporation."
+global.botscript = 'https://github.com/Bbn-hub/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🗿Demzzy\n\n+2349091878122"
+global.creator = "2349091878122@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["2349091878122"] // Premium User
 global.hituet = 0
 
 //bot sett
